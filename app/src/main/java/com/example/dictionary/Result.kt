f0a82dc0d7d05d0ccc6cd3fde5dce0eec2b307d7
Mohammad Atif Quamar <atif20523@iiitd.ac.in>
2023-03-18 10:58:26 +0530

    fixed word viewing on result page

--- app/src/main/java/com/example/dictionary/Result.kt
+++ app/src/main/java/com/example/dictionary/Result.kt
@@ -3,38 +3,28 @@ package com.example.dictionary
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.dictionary.fragments.Audio
 import com.example.dictionary.fragments.ListOfMeanings
 import org.json.JSONArray
+import java.util.*
 
 class Result : AppCompatActivity() {
 
+    private lateinit var wordTxt: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.i("Message in result", "Just before setContentView")
         setContentView(R.layout.activity_result)
-        Log.i("Message in result", "Just after setContentView")
         val jsonString = intent.getStringExtra("jsonArray")
-        if (jsonString != null) {
-            Log.i("Length of string in result.kt", "${jsonString.length}")
-        }
         val jsonArray = JSONArray(jsonString)
         val jsonObject = jsonArray.getJSONObject(0)
-        val getUrl = jsonObject.getString("sourceUrls")
-        Log.i("Message in result", "Word found and the sourceUrls is $getUrl")
+        val getName = jsonObject.getString("word")
+        Log.i("Message in result", "Word found and the word is $getName")
 
-//        val listOfMeaningsInstance = ListOfMeanings()
-//        val args = Bundle()
-//        args.putString("jsonArray", jsonString)
-//        listOfMeaningsInstance.arguments = args
-//        supportFragmentManager.beginTransaction().replace(R.id.activity_result, listOfMeaningsInstance).commit()
-
-//        val audioInstance = Audio()
-//        val args = Bundle()
-//        args.putString("jsonString", jsonString)
-//        audioInstance.arguments = args
-//        supportFragmentManager.beginTransaction().add(R.id.fragmentContainerView, audioInstance).commit()
+        wordTxt = findViewById(R.id.idWord)
+        wordTxt.text = getName.capitalize(Locale.ROOT)
 
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
